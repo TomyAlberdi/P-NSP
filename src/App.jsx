bbc98644fdfd9bfd1c7a675a308ec8bd98889182
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2023-06-13 21:23:15 -0300

    Set Project Structure 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/pages/Home'
+
 function App() {
   return (
     <div className="App">
-      <h1>app</h1>
+      <main>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </main>
     </div>
   )
 }
